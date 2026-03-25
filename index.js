cd79194e6d3d919be5cde3f454dda77ca453d6d3
MitGohel25 <mit.gohel1325@gmail.com>
2026-03-25 06:22:19 +0530

    Added EJS UI, URL generation display, and analytics table

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const path = require("path");
 const { connectToMongoDB } = require("./connection");
 const urlRoute = require("./routes/url");
+const staticRoute = require("./routes/staticRouter");
 const URL = require("./models/url");
 
 const app = express();
@@ -10,11 +12,16 @@ connectToMongoDB("mongodb://localhost:27017/short-url").then(() =>
   console.log("Mongodb connected"),
 );
 
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
 
 app.use("/url", urlRoute);
+app.use("/", staticRoute);
 
-app.get("/:shortId", async (req, res) => {
+app.get("/url/:shortId", async (req, res) => {
   const shortId = req.params.shortId;
   const entry = await URL.findOneAndUpdate(
     {
@@ -28,6 +35,10 @@ app.get("/:shortId", async (req, res) => {
       },
     },
   );
+  if (!entry) {
+    return res.status(404).send("Short URL not found");
+  }
+
   res.redirect(entry.redirectURL)
 });
 
